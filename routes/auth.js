2b9e1777c747a2f7b683c5308534f73a63eb4ed6
Temitope Jegede <temiteejegede@gmail.com>
2022-06-02 18:14:07 -0400

    minor errors

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.router();
 
 const { login, register } = require("../controllers/auth");
 
-router.pst("/register", register);
+router.post("/register", register);
 router.post("/login", login);
 
 module.exports = router;
