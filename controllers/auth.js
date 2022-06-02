81d3438bb79ccb64b5809daf3231379cb1b94302
Temitope Jegede <temiteejegede@gmail.com>
2022-06-01 23:47:43 -0400

    added register controller

---
+++ controllers/auth.js
@@ -0,0 +1,12 @@
+const register = async (req, res) => {
+  res.send("register user");
+};
+
+const login = (req, res) => {
+  res.send("login user");
+};
+
+module.exports = {
+  register,
+  login,
+};
